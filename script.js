86612882c7e17d6b2916388c8238857ddfa6b0ba
Karolm0406 <Kherreram2200@alumno.ipn.mx>
2024-09-18 20:59:15 -0600

    18/09/2024 Karol

---
+++ script.js
@@ -0,0 +1,34 @@
+
+//18 09 2024 /Herrera Macedo Karol Marilú
+//comenrtario en una sola linea
+/*comentario multi lineas*/
+
+//VARIABLES
+/*LAS VARIABLES GUARDAN INORMACIÓN
+QUE PUEDES RESUTILIZAR
+ */
+
+//Declaración de variables 
+let nombre= "Karol";
+const PI=3.1416;
+var= edad=25;
+
+/*Tipos de datos:
+Cadena de taxto (string): "Hola, mundo"
+Número (number): 5,13,14
+Booleanos(boolean);true,false
+*/
+
+//EJEMPLO
+let mensaje="Hola mundo";
+let numero=42;
+let Estudiante=true;
+
+//operdores matemáticos
+let a=5;
+let b=2;
+let suma=a+b;//7
+let resta=a-b;//3
+let multiplicacion=a*b;//10
+let division=a/b;//2.5
+
