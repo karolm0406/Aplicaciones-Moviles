f5c9986bbe8680ba68f32465d23277637efc8fc4
unknown <ah5008034@gmail.com>
2024-10-16 20:45:09 -0600

    agregue

---
+++ Clase_16_10_24/script.js
@@ -0,0 +1,11 @@
+//console.log("buenas noches");
+const h1 = document.createElement("h1");
+h1.textContent = "Hola Clase de DOM desde js";
+
+//console.log(h1.textContent);
+document.body.appendChild(h1);
+
+
+
+
+
